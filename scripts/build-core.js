0cb7878336e882c18831545a2ead16b3b21c6d45
Kyle Simpson <getify@gmail.com>
2022-08-27 10:13:31 -0500

    bumping versions of terser, qunit, and coveralls dependencies, updating build script for (much) newer terser

--- scripts/build-core.js
+++ scripts/build-core.js
@@ -2,7 +2,7 @@
 
 var fs = require("fs"),
 	path = require("path"),
-	ugly = require("terser"),
+	terser = require("terser"),
 	packageJSON,
 	copyrightHeader,
 	version,
@@ -22,58 +22,58 @@ var fs = require("fs"),
 console.log("*** Building Core ***");
 console.log(`Building: ${CORE_DIST}`);
 
-try {
-	// try to make the dist directory, if needed
+(async function main(){
 	try {
-		fs.mkdirSync(DIST_DIR,0o755);
-	}
-	catch (err) { }
-
-	result += fs.readFileSync(CORE_SRC,{ encoding: "utf8" });
-
-	result = ugly.minify(result,{
-		mangle: {
-			keep_fnames: true
-		},
-		compress: {
-			keep_fnames: true
-		},
-		output: {
-			comments: /^!/
+		// try to make the dist directory, if needed
+		try {
+			fs.mkdirSync(DIST_DIR,0o755);
 		}
-	});
+		catch (err) { }
 
-	// was compression successful?
-	if (!(result && result.code)) {
-		if (result.error) throw result.error;
-		else throw result;
-	}
+		result += fs.readFileSync(CORE_SRC,{ encoding: "utf8" });
 
-	// read version number from package.json
-	packageJSON = JSON.parse(
-		fs.readFileSync(
-			path.join(ROOT_DIR,"package.json"),
+		result = await terser.minify(result,{
+			mangle: {
+				keep_fnames: true,
+			},
+			compress: {
+				keep_fnames: true,
+			},
+			output: {
+				comments: /^!/,
+			},
+		});
+		if (!(result && result.code)) {
+			if (result.error) throw result.error;
+			else throw result;
+		}
+
+		// read version number from package.json
+		packageJSON = JSON.parse(
+			fs.readFileSync(
+				path.join(ROOT_DIR,"package.json"),
+				{ encoding: "utf8" }
+			)
+		);
+		version = packageJSON.version;
+
+		// read copyright-header text, render with version and year
+		copyrightHeader = fs.readFileSync(
+			path.join(SRC_DIR,"copyright-header.txt"),
 			{ encoding: "utf8" }
-		)
-	);
-	version = packageJSON.version;
+		).replace(/`/g,"");
+		copyrightHeader = Function("version","year",`return \`${copyrightHeader}\`;`)( version, year );
 
-	// read copyright-header text, render with version and year
-	copyrightHeader = fs.readFileSync(
-		path.join(SRC_DIR,"copyright-header.txt"),
-		{ encoding: "utf8" }
-	).replace(/`/g,"");
-	copyrightHeader = Function("version","year",`return \`${copyrightHeader}\`;`)( version, year );
+		// append copyright-header text
+		result = `${copyrightHeader}${result.code}`;
 
-	// append copyright-header text
-	result = `${copyrightHeader}${result.code}`;
+		// write dist
+		fs.writeFileSync( CORE_DIST, result, { encoding: "utf8" } );
 
-	// write dist
-	fs.writeFileSync( CORE_DIST, result, { encoding: "utf8" } );
-
-	console.log("Complete.");
-}
-catch (err) {
-	console.error(err);
-	process.exit(1);
-}
+		console.log("Complete.");
+	}
+	catch (err) {
+		console.error(err);
+		process.exit(1);
+	}
+})();
